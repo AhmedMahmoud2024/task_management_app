99be11e5c4b8f89a2d58a518da04d0deeade29d7
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-05-23 08:02:42 -0700

    feat:created project structure

---
+++ lib/task_management/data/models/task_model.dart
@@ -0,0 +1,25 @@
+class TaskModel{
+  final String id;
+  final String title;
+
+  TaskModel({
+    required this.id,
+    required this.title
+  });
+
+factory TaskModel.fromMap(Map<String,dynamic>map){
+  return TaskModel(
+    id: map['id'] ?? '',
+   title: map['title'] ?? ''
+   );
+
+  }
+
+  Map<String,dynamic> toMap(){
+    return{
+      'id':id,
+      'title':title
+    };
+
+  }
+}
