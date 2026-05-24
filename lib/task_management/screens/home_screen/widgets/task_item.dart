6fd646bfbf428bcbe81ced3f58b1ae36d96067c0
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-05-24 04:23:39 -0700

    feat:modified task_item widget with leading checkbox for toggle functionality

--- lib/task_management/screens/home_screen/widgets/task_item.dart
+++ lib/task_management/screens/home_screen/widgets/task_item.dart
@@ -3,13 +3,24 @@ import 'package:task_management_app/task_management/data/models/task_model.dart'
 
 class TaskItem extends StatelessWidget {
   final TaskModel task;
-  const TaskItem({super.key, required this.task});
+  final VoidCallback onToggle;
+  const TaskItem({super.key, required this.task,required this.onToggle});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(task.title),
+        leading: Checkbox(
+          value:task.isCompleted,
+          onChanged:(_)=>onToggle()
+        ),
+        title: Text(
+           task.title,
+         style:TextStyle(
+          decoration: task.isCompleted?TextDecoration.lineThrough
+          :null
+         )
+        ),
       ),
     );
   }
