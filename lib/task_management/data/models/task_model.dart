7f180e3ff3231a3c9e21ff80c9afed42e532b65b
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-05-24 04:16:19 -0700

    feat:modified task model for toggle functionality

--- lib/task_management/data/models/task_model.dart
+++ lib/task_management/data/models/task_model.dart
@@ -1,16 +1,30 @@
 class TaskModel{
   final String id;
   final String title;
-
+  final bool isCompleted;
   TaskModel({
     required this.id,
-    required this.title
+    required this.title,
+    this.isCompleted=false
   });
+
+  TaskModel copyWith({
+  String? id,
+  String? title,
+  bool? isCompleted
+  }){
+    return TaskModel(
+      id:id ?? this.id,
+      title:title ?? this.title,
+      isCompleted:isCompleted ?? this.isCompleted
+    );
+  }
 
 factory TaskModel.fromMap(Map<String,dynamic>map){
   return TaskModel(
     id: map['id'] ?? '',
-   title: map['title'] ?? ''
+   title: map['title'] ?? '',
+   isCompleted: map['isCompleted'] ?? false 
    );
 
   }
@@ -18,7 +32,8 @@ factory TaskModel.fromMap(Map<String,dynamic>map){
   Map<String,dynamic> toMap(){
     return{
       'id':id,
-      'title':title
+      'title':title,
+      'isCompleted':isCompleted
     };
 
   }
